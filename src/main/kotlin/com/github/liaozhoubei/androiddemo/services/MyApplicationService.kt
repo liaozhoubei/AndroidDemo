4d4b3726b80275f89ea9dff4441c0419468167e4
GitHub Action <action@github.com>
2021-05-20 09:34:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/liaozhoubei/androiddemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liaozhoubei/androiddemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liaozhoubei.androiddemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liaozhoubei.androiddemo.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/liaozhoubei/androiddemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/liaozhoubei/androiddemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.liaozhoubei.androiddemo.services
 
+import com.github.liaozhoubei.androiddemo.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
